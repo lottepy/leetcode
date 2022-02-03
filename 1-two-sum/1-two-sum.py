da0774cf8130dd101cc67114b8c5733c372aa907
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-03 22:34:30 +0800

    Time: 1079 ms (32.04%), Space: 15.3 MB (53.31%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,24 @@
 class Solution:
+    # # Hint 2
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     counterNums = [target - i for i in nums]
+    #     result = []
+    #     for i, num in enumerate(counterNums):
+    #         if num in nums:
+    #             counterIdx = nums.index(num)
+    #             if i != counterIdx:
+    #                 return [i, counterIdx]
+    
+    
+    # # Hint 3
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        counterNums = [target - i for i in nums]
-        result = []
-        for i, num in enumerate(counterNums):
-            if num in nums:
-                counterIdx = nums.index(num)
-                if i != counterIdx:
-                    return [i, counterIdx]
+        hashMap = dict()
+        for i, num in enumerate(nums):
+            residual = target - num
+            if residual in list(hashMap.keys()):
+                return sorted([i, hashMap[residual]], reverse=False)
+            else:
+                hashMap[num] = i
             
 if __name__ == '__main__':
     sol = Solution()
