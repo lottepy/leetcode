9109df89e520ebfa448c4105919f11de54fdeecd
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-13 16:52:23 +0800

    Time: 6084 ms (15.31%), Space: 22 MB (8.88%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,31 +1,43 @@
 class Solution:
-    # # Method 1: Recursion
-    # def longestPalindrome(self, s: str) -> str:
-    #     if s == s[::-1]:
-    #         return s
-    #     else:
-    #         lower = self.longestPalindrome(s[:-1])
-    #         upper = self.longestPalindrome(s[1:])
-    #         return lower if len(lower) > len(upper) else upper
-     
-    # Method 2: Tabeulation
+    # Method 1: DP Tabulation
     def longestPalindrome(self, s: str) -> str:
-        def palindrome(left, right):
-            while left >= 0 and right <= len(s) - 1 and s[left] == s[right]:
-                left -= 1
-                right += 1
-            return s[left+1:right]    
-
         res = s[0]
-        for l in range(len(s) - 1):
-            odd = palindrome(l, l)
-            even = palindrome(l, l+1)
-            if max(len(odd), len(even)) > len(res):
-                res = odd if len(odd) > len(even) else even
-        return res        
+        dp = [[False] * len(s) for _ in range(len(s))]
+        for i in range(len(s)):
+            dp[i][i] = True
+            if i < len(s) - 1:
+                if s[i+1] == s[i]:
+                    dp[i][i+1] = True
+                    res = s[i:i+2] if len(s[i:i+2]) > len(res) else res
+
+        for l in range(2,len(s)):
+            for i in range(len(s)-l):
+                j = i + l
+                if dp[i+1][j-1] and s[i] == s[j]:
+                    dp[i][j] = True
+                    res = s[i:j+1] if len(s[i:j+1]) > len(res) else res
+        return res
+
+
+    # # Method 2: Expansion
+    # def longestPalindrome(self, s: str) -> str:
+    #     def palindrome(substr):
+    #         if len(substr) < 2:
+    #             return True
+    #         if substr[0] != substr[-1]:
+    #             return False
+    #         return palindrome(substr[1:-1])    
+
+    #     res = s[0]
+    #     for l in range(len(s) - 1):
+    #         odd = palindrome(l, l)
+    #         even = palindrome(l, l+1)
+    #         if max(len(odd), len(even)) > len(res):
+    #             res = odd if len(odd) > len(even) else even
+    #     return res        
 
 
 if __name__ == '__main__':
-    s = 'abbca'
+    s = 'cbbd'
     sol = Solution()
-    print(sol.longestPalindrome(s))           
+    print(sol.longestPalindrome(s))        
