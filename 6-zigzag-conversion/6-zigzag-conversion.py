e07c733235bb82f6af995550a39d525e6380af76
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-14 20:52:24 +0800

    Time: 86 ms (49.30%), Space: 13.9 MB (94.17%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.py
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -1,23 +1,38 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        # # Method 1: Store in matrix
+        # if numRows == 1:
+        #     return s
+        # dp = [[''] * (len(s) // (numRows - 1) + 1) for _ in range(numRows)]
+        # count, i = 0, 0
+        # while i < len(s):
+        #     if count % numRows == 0 and i != 0:
+        #         i -= 1
+        #     else:    
+        #         if count // numRows % 2 == 0:
+        #             dp[count % numRows][count // numRows] = s[i]
+        #         else:
+        #             dp[numRows - count % numRows - 1][count // numRows] = s[i]        
+        #     count += 1 
+        #     i += 1
+               
+        # return ''.join([''.join(r) for r in dp])
+
+        # Method 2: 
         if numRows == 1:
             return s
-        dp = [[''] * (len(s) // (numRows - 1) + 1) for _ in range(numRows)]
-        count, i = 0, 0
-        while i < len(s):
-            if count % numRows == 0 and i != 0:
-                i -= 1
-            else:    
-                if count // numRows % 2 == 0:
-                    dp[count % numRows][count // numRows] = s[i]
-                else:
-                    dp[numRows - count % numRows - 1][count // numRows] = s[i]        
-            count += 1 
-            i += 1
-               
-        return ''.join([''.join(r) for r in dp])
+
+        res = ''
+        for r in range(numRows):
+            increment = 2 * (numRows - 1)
+            for i in range(r, len(s), increment):
+                res += s[i]
+                if r % (numRows - 1) != 0 and i + increment - 2 * r < len(s):
+                    res += s[i + increment - 2 * r]
+        return res            
+
 
 if __name__ == '__main__':
     s = "paypalishiring"
     sol = Solution()
-    print(sol.convert(s, 2))        
+    print(sol.convert(s, 2))         
