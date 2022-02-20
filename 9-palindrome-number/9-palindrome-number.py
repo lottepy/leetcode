0eabc38e4bdd348aa348e2ea3076f91055c96668
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-20 11:55:57 +0800

    Time: 64 ms (80.62%), Space: 13.9 MB (86.41%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,14 +1,28 @@
 import copy
 class Solution:
+    # # Method 1: revert
+    # def isPalindrome(self, x: int) -> bool:
+    #     if x < 0:
+    #         return False
+
+    #     y = copy.deepcopy(x)
+    #     _x = 0
+    #     while x != 0:    
+    #         _x = _x * 10 + x % 10
+    #         x = x // 10
+    #     if _x == y:
+    #         return True    
+    #     return False 
+
+    # Method 2: revert half       
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
+        if (x < 0) or (x % 10 == 0 and x != 0):
             return False
 
-        y = copy.deepcopy(x)
         _x = 0
-        while x != 0:    
+        while x > _x:    
             _x = _x * 10 + x % 10
             x = x // 10
-        if _x == y:
+        if x == _x // 10 or x == _x:
             return True    
         return False    
