c3ae635cd3fb0716b93b9f9a886de501b97689ad
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-20 20:02:19 +0800

    Time: 47 ms (51.83%), Space: 13.9 MB (96.56%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,14 +1,29 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        if len(strs) == 0:
-            return ''
+    # # Method 1: recursion
+    # def longestCommonPrefix(self, strs: List[str]) -> str:
+    #     if len(strs) == 0:
+    #         return ''
         
-        if len(strs[0]) != 0:
-            ch = strs[0][0]
-        else:
-            return ''
-        for i in range(1, len(strs)):
-            if len(strs[i]) == 0 or strs[i][0] != ch:
-                return ''
+    #     if len(strs[0]) != 0:
+    #         ch = strs[0][0]
+    #     else:
+    #         return ''
+    #     for i in range(1, len(strs)):
+    #         if len(strs[i]) == 0 or strs[i][0] != ch:
+    #             return ''
         
-        return ch + self.longestCommonPrefix([s[1:] for s in strs])
+    #     return ch + self.longestCommonPrefix([s[1:] for s in strs])
+
+    # Method 2: sorting
+    def longestCommonPrefix(self, strs):
+        if not strs: return ""
+        if len(strs) == 1: return strs[0]
+        
+        strs.sort()
+        p = ""
+        for x, y in zip(strs[0], strs[-1]):
+            if x == y: 
+                p+=x
+            else: 
+                break
+        return p
