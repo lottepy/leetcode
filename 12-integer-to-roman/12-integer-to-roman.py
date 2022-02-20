ad0e91d1777e63103c0a8fa6d5b9e0f33bae5649
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-20 22:29:35 +0800

    Time: 81 ms (34.35%), Space: 13.9 MB (94.33%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,4 +1,5 @@
 class Solution:
+    # Method 1: Brute Force
     def intToRoman(self, num: int) -> str:
         romanMap = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
         invRomanMap = {v: k for k, v in romanMap.items()}
