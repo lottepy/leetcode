f24a272fb3115a5c8e55c832a30fb42926efad0c
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-20 14:40:04 +0800

    Time: 90 ms (39.98%), Space: 14.3 MB (56.77%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.py
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -1,26 +1,33 @@
 class Solution:
-    # Method 1: recursion
-    def isMatch(self, s: str, p: str) -> bool:
-        if not p:
-            return not s    
-        match = bool(s) and ((s[0] == p[0]) or (p[0] == '.'))
-        if len(p) >= 2 and p[1] == '*':
-            return (match and self.isMatch(s[1:], p)) or self.isMatch(s, p[2:])
-        else:
-            return match and self.isMatch(s[1:], p[1:])  
+    # # Method 1: recursion
+    # def isMatch(self, s: str, p: str) -> bool:
+    #     if not p:
+    #         return not s    
+    #     match = bool(s) and ((s[0] == p[0]) or (p[0] == '.'))
+    #     if len(p) >= 2 and p[1] == '*':
+    #         return (match and self.isMatch(s[1:], p)) or self.isMatch(s, p[2:])
+    #     else:
+    #         return match and self.isMatch(s[1:], p[1:])  
 
-#     # Method 2: iteration
-#     def isMatch(self, s: str, p: str) -> bool:
-#         def dfs(i, j):
-#             if i >= len(s) and j >= len(p):
-#                 return True
-#             if j >= len(p):
-#                 return False    
-#             match = i < len(s) and (s[i] == p[j] or p[j] == '.')
-#             if j + 1 < len(p) and p[j + 1] == '*':
-#                 return (match and dfs(i+1, j)) or dfs(i, j+2)
-#             if match:
-#                 return dfs(i+1, j+1)    
-#             return False    
+    # Method 2: iteration
+    def isMatch(self, s: str, p: str) -> bool:
+        memo = dict()
+        def dfs(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if i >= len(s) and j >= len(p):
+                memo[(i, j)] = True
+                return True
+            if j >= len(p):
+                memo[(i, j)] = False
+                return False    
+            
+            match = i < len(s) and (s[i] == p[j] or p[j] == '.')
+            if j + 1 < len(p) and p[j + 1] == '*':
+                memo[(i, j)] = (match and dfs(i+1, j)) or dfs(i, j+2)
+                return memo[(i, j)]
+
+            memo[(i, j)] = match and dfs(i+1, j+1)    
+            return memo[(i, j)]    
                   
-#         return dfs(0, 0)
+        return dfs(0, 0)
