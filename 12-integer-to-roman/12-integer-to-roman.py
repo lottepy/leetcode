f4a7f014b7ad55739d801a300bda8dc7ce92338e
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-21 12:29:14 +0800

    Time: 71 ms (49.55%), Space: 13.8 MB (94.13%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,30 +1,42 @@
 class Solution:
-    # Method 1: Brute Force
+    # # Method 1: Brute Force
+    # def intToRoman(self, num: int) -> str:
+    #     romanMap = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
+    #     invRomanMap = {v: k for k, v in romanMap.items()}
+    #     subtractionMap = {'V': 'I', 'X': 'I', 'L': 'X', 'C': 'X', 'D': 'C', 'M': 'C'}
+    #     elements = list(romanMap.keys())
+    #     elements.sort()
+    #     res = ''
+    #     pos = len(elements) - 1
+    #     while num != 0 and pos >= 0:
+    #         residual = num % elements[pos]
+    #         multi = num // elements[pos]
+    #         subtraction = (multi == 0 and elements[pos] - residual <= invRomanMap[subtractionMap[romanMap[elements[pos]]]])  
+    #         if multi != 0 or subtraction:
+    #             if multi != 0:
+    #                 res += romanMap[elements[pos]] * multi    
+    #                 num = residual
+    #             elif subtraction:
+    #                 res += subtractionMap[romanMap[elements[pos]]] + romanMap[elements[pos]]
+    #                 num = num % invRomanMap[subtractionMap[romanMap[elements[pos]]]]
+    #         else:
+    #             pos -= 1    
+    #     return res
+
+    # Method 2: Construct mapping
     def intToRoman(self, num: int) -> str:
-        romanMap = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
-        invRomanMap = {v: k for k, v in romanMap.items()}
-        subtractionMap = {'V': 'I', 'X': 'I', 'L': 'X', 'C': 'X', 'D': 'C', 'M': 'C'}
+        romanMap = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
         elements = list(romanMap.keys())
         elements.sort()
         res = ''
         pos = len(elements) - 1
+
         while num != 0 and pos >= 0:
             residual = num % elements[pos]
             multi = num // elements[pos]
-            # if residual > 10:
-            subtraction = (multi == 0 and elements[pos] - residual <= invRomanMap[subtractionMap[romanMap[elements[pos]]]])
-            # else:
-            #     subtraction = ((pos == 2 and residual == 9) or (pos == 1 and residual == 4))    
-            if multi != 0 or subtraction:
-                if multi != 0:
-                    res += romanMap[elements[pos]] * multi    
-                    num = num % elements[pos]
-                elif subtraction:
-                    res += subtractionMap[romanMap[elements[pos]]] + romanMap[elements[pos]]
-                    num = num % invRomanMap[subtractionMap[romanMap[elements[pos]]]]
+            if multi != 0:
+                res += romanMap[elements[pos]] * multi  
+                num = residual
             else:
-                pos -= 1    
+                pos -= 1
         return res
-        
-if __name__ == '__main__':
-    print(Solution().intToRoman(8))
