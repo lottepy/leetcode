4437afa7c853e39c50d97646eca776ce43d7d87e
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-22 12:56:04 +0800

    Time: 156 ms (24.35%), Space: 14.9 MB (14.60%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,9 +1,23 @@
 class Solution:
+    # # Method 1: hash
+    # def twoSum(self, nums: list[int], target: int) -> list[int]:
+    #     hash = dict()
+    #     for i in range(len(nums)):
+    #         if nums[i] in hash:
+    #             return [hash[nums[i]], i+1]
+    #         else:    
+    #             hash[target - nums[i]] = i + 1
+    #     return []   
+
+    # Method 2: Two pointers
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        hash = dict()
-        for i in range(len(nums)):
-            if nums[i] in hash:
-                return [hash[nums[i]], i+1]
-            else:    
-                hash[target - nums[i]] = i + 1
-        return []        
+        l, r = 0, len(nums)-1
+        while l < r:
+            _sum = nums[l] + nums[r]
+            if _sum < target:
+                l += 1
+            elif _sum > target:
+                r -= 1
+            else:
+                return [l+1, r+1]    
+        return []         
