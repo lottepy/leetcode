cb46840bf5d2f8804f2b816717a41f1f2fd104bc
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-22 12:52:24 +0800

    Time: 1838 ms (34.18%), Space: 18 MB (50.74%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,3 +1,4 @@
+
 import copy
 class Solution:
     # # Method 1: Brute Force
@@ -27,38 +28,60 @@ class Solution:
     #             continue   
     #     return list([list(t) for t in res.keys()])    
 
-    # Method 2: Sorted TwoSum
+    # # Method 2: Sort and TwoSum
+    # def threeSum(self, nums: list[int]) -> list[list[int]]:
+    #     def twoSum(_nums: list[int], target: int, i: int) -> list[list[int]]:
+    #         _nums.pop(i)
+    #         _hash = dict()
+    #         _res = []
+    #         for i in range(len(_nums)):
+    #             if _nums[i] in _hash:
+    #                 _res.append([_hash[_nums[i]], _nums[i]])
+    #             else:
+    #                 _hash[target - _nums[i]] = _nums[i]   
+    #         return _res       
+
+    #     res = dict()
+    #     nums.sort()
+    #     targetPool = set()
+    #     for i, num in enumerate(nums):
+    #         target = -num
+    #         if target not in targetPool:
+    #             targetPool.add(target)
+    #             meetTarget = twoSum(copy.deepcopy(nums), target, i)
+    #             if len(meetTarget) > 0:
+    #                 for meet in meetTarget:
+    #                     meet.append(num)
+    #                     meet.sort()
+    #                     res[tuple(meet)] = 0        
+    #         else:
+    #             continue            
+
+    #     return [list(k) for k in res.keys()]
+
+    # Method 3: Sort and Two Pointers
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        def twoSum(_nums: list[int], target: int, i: int) -> list[list[int]]:
-            _nums.pop(i)
-            _hash = dict()
-            _res = []
-            for i in range(len(_nums)):
-                if _nums[i] in _hash:
-                    _res.append([_hash[_nums[i]], _nums[i]])
-                else:
-                    _hash[target - _nums[i]] = _nums[i]   
-            return _res       
-
-        res = dict()
         nums.sort()
-        targetPool = set()
-        for i, num in enumerate(nums):
-            target = -num
-            if target not in targetPool:
-                targetPool.add(target)
-                meetTarget = twoSum(copy.deepcopy(nums), target, i)
-                if len(meetTarget) > 0:
-                    for meet in meetTarget:
-                        meet.append(num)
-                        meet.sort()
-                        res[tuple(meet)] = 0         
-            else:
-                continue            
-
-        return [list(k) for k in res.keys()]
-
-
-
-if __name__ == '__main__':
-    print(Solution().threeSum([-1,0,1,2,-1,-4,-2,-3,3,0,4]))
+        
+        res = []
+        i = 0
+        for i in range(len(nums)):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+            left, right = i+1, len(nums) - 1
+            target = - nums[i]
+            while left < right:
+                if left == i:
+                    left += 1
+                if right == i:
+                    right -= 1          
+                if nums[left] + nums[right] == target:
+                    res.append([nums[left], nums[right], nums[i]])
+                    left += 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1 
+                elif nums[left] + nums[right] > target:
+                    right -= 1
+                elif nums[left] + nums[right] < target:
+                    left += 1 
+        return res
