d28e79acf58f6f04df91be4ff31761aa7894a43b
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-23 21:26:32 +0800

    Time: 240 ms (50.08%), Space: 13.9 MB (96.71%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -5,8 +5,6 @@ class Solution:
         nums.sort()
         res = sum([smallest for smallest in nums[:3]])
         for i, t in enumerate(nums):
-            # if i == 0 and nums[i] > target:
-            #     break
             if i > 0 and t == nums[i-1]:
                 continue
             l, r = i + 1, len(nums) - 1
