2263beb3f71e0a67c15215c3b630b1d0131285a2
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-25 21:08:50 +0800

    Time: 89 ms (5.07%), Space: 14 MB (52.12%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -5,22 +5,40 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        res = copy.copy(head)
-        forCount = copy.copy(head)
-        prev = ListNode(None)
-        prev.next = res
-        count = 0
-        while forCount.next is not None:
-            forCount = forCount.next
-            count += 1
+    # # Method 1: Brute Force
+    #     res = copy.copy(head)
+    #     forCount = copy.copy(head)
+    #     prev = ListNode(None)
+    #     prev.next = res
+    #     count = 0
+    #     while forCount.next is not None:
+    #         forCount = forCount.next
+    #         count += 1
 
-        _count = 0
-        while _count <= count - n:
-            prev = prev.next
-            head = head.next
-            _count += 1
+    #     _count = 0
+    #     while _count <= count - n:
+    #         prev = prev.next
+    #         head = head.next
+    #         _count += 1
 
-        prev.next = head.next
-        if prev.val is None:
-            return prev.next
-        return res
+    #     prev.next = head.next
+    #     if prev.val is None:
+    #         return prev.next
+    #     return res
+    
+    # Method 2: Two Pointers    
+        front, end = 1, 1
+        _head = ListNode(None)
+        _head.next = head
+        res = _head
+        tail = copy.deepcopy(head)
+        while end - front <= n:
+            if end - front == n and tail is not None:
+                _head = _head.next
+                front += 1
+            elif end - front == n and tail is None:
+                _head.next = _head.next.next
+                return res.next
+            tail = tail.next 
+            end += 1
+        return None        
