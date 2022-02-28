74641646e571c2523c56a0c500221e84861a3d93
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-28 21:32:41 +0800

    Time: 28 ms (95.89%), Space: 13.8 MB (98.17%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -4,19 +4,37 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def swapPairs(self, head: ListNode) -> ListNode:
-        swap = ListNode(None)
-        res = swap
-        count = 0
-        while head is not None:
-            if head.next is None:
-                swap.next = head
-                head = head.next
-            else:
-                swap.next = ListNode(head.next.val)
-                swap.next.next = ListNode(head.val)
-                head = head.next.next
-                swap = swap.next.next
+    # # Method 1: Brute Force
+    # def swapPairs(self, head: ListNode) -> ListNode:
+    #     swap = ListNode(None)
+    #     res = swap
+    #     while head is not None:
+    #         if head.next is None:
+    #             swap.next = head
+    #             head = head.next
+    #         else:
+    #             swap.next = ListNode(head.next.val)
+    #             swap.next.next = ListNode(head.val)
+    #             head = head.next.next
 
-        return res.next        
-            
+    #     return res.next 
+
+    # Method 2: Shift Pointers          
+        def swapPairs(self, head: ListNode) -> ListNode: 
+            res = ListNode(0, head)
+            prev, curr = res, head
+            while curr and curr.next:
+                # Store pointers
+                next = curr.next
+                followingPair = curr.next.next
+
+                # Change pointers
+                next.next = curr
+                prev.next = next
+                curr.next = followingPair
+
+                # Reassign
+                prev = curr
+                curr = followingPair
+
+            return res.next
