4d70622ffe02621669c476818440ef4427dc1fdb
lottepy <98939371+lottepy@users.noreply.github.com>
2022-02-28 21:07:21 +0800

    Time: 28 ms (99.48%), Space: 14 MB (67.71%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -1,40 +1,29 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None and list2 is None:
+    def mergeKLists(self, lists: list[ListNode]) -> ListNode:
+        if not lists or len(lists) == 0:
             return None
-        elif (list1 is None and list2 is not None) or (list1 is not None and list2 is not None and list1.val > list2.val):
-            return self.mergeTwoLists(list2, list1)
-        anchor1, anchor2 = list1, list2       
-        res = anchor1
-        while anchor1 is not None and anchor2 is not None:  
-            if anchor1.val <= anchor2.val:
-                if anchor1.next is None:
-                    anchor1.next = anchor2
-                    anchor2 = None
-                elif anchor2.val <= anchor1.next.val:    
-                    temp = copy.deepcopy(anchor1.next)
-                    anchor1.next = ListNode(anchor2.val)
-                    anchor1.next.next = temp
-                    anchor1 = anchor1.next
-                    anchor2 = anchor2.next  
-                else:
-                    anchor1 = anchor1.next    
-            else: # anchor1.val > anchor2.val
-                pass
+        while len(lists) > 1:
+            merged = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1) < len(lists) else None
+                merged.append(self.mergeTwoLists(l1, l2))
+            lists = merged    
 
-        return res          
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode):
+        merge = ListNode(None)      
+        res = merge
+        while list1 is not None and list2 is not None:
+            if list1.val <= list2.val:
+                merge.next = ListNode(list1.val)
+                list1 = list1.next
+            else:
+                merge.next = ListNode(list2.val)
+                list2 = list2.next
+            merge = merge.next
 
-
-if __name__ == '__main__':
-    list1 = ListNode(-9)
-    list1.next = ListNode(3)
-    # list1.next.next = ListNode(4)
-    list2 = ListNode(5)
-    list2.next = ListNode(7)
-    # list2.next.next = ListNode(4)
-    print(Solution().mergeTwoLists(list1, list2))
+        if list1 is not None:
+            merge.next = list1
+        if list2 is not None:
+            merge.next = list2
+        return res.next        
