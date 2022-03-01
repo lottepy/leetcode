5d43140afb12423bb60c1c6efa1b321e3f11bbe3
lottepy <98939371+lottepy@users.noreply.github.com>
2022-03-01 15:21:19 +0800

    Time: 1089 ms (5.17%), Space: 37.3 MB (5.24%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
@@ -5,42 +5,62 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if k == 1:
+        # # Method1: Space O(k)
+        # if k == 1:
+        #     return head
+        # res = ListNode(0, head)
+        # prev, curr = res, head
+
+        # def updateK(curr):
+        #     next = dict()
+        #     i = 1
+        #     next[i] = curr
+        #     while i < k:
+        #         if curr and curr.next:
+        #             curr = curr.next
+        #             next[i+1] = curr
+        #             i += 1
+        #         else:
+        #             return None
+        #     return next      
+        
+        # next = updateK(curr)
+        # if not next:
+        #     return res.next
+
+        # followingK = next[k].next
+        # while next[k]:
+        #     # Change pointers
+        #     for j in range(k, 1, -1):
+        #         next[j].next = next[j - 1] 
+        #     curr.next = followingK
+        #     prev.next = next[k]
+        
+        #     # Re-Initiate
+        #     prev = curr
+        #     curr = followingK
+        #     next = updateK(curr)       
+        #     if not next:
+        #         return res.next
+        #     followingK = next[k].next
+
+        # return res.next   
+
+        # Method2: Space O(1) Recursion
+        if k == 1 or not head:
             return head
-        res = ListNode(0, head)
-        prev, curr = res, head
-
-        def updateK(curr):
-            next = dict()
-            i = 1
-            next[i] = curr
-            while i < k:
-                if curr and curr.next:
-                    curr = curr.next
-                    next[i+1] = curr
-                    i += 1
-                else:
-                    return None
-            return next      
-        
-        next = updateK(curr)
-        if not next:
-            return res.next
-
-        followingK = next[k].next
-        while next[k]:
-            # Change pointers
-            for j in range(k, 1, -1):
-                next[j].next = next[j - 1] 
-            curr.next = followingK
-            prev.next = next[k]
-        
-            # Re-Initiate
+        base = copy.deepcopy(head)    
+        prev, curr = None, head
+        i, next = 0, None
+        while curr and i < k:
+            next = curr.next
+            curr.next = prev
             prev = curr
-            curr = followingK
-            next = updateK(curr)       
-            if not next:
-                return res.next
-            followingK = next[k].next
+            curr = next
+            i += 1
+        if i < k:
+            return base      
+        elif next:      
+            head.next = self.reverseKGroup(next, k)  
 
-        return res.next 
+        return prev 
