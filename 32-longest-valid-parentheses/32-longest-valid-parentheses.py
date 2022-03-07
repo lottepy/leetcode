cdcaba1ed932e915d92c5535a93788dfd970833b
lottepy <98939371+lottepy@users.noreply.github.com>
2022-03-07 14:28:19 +0800

    Time: 62 ms (55.32%), Space: 14.1 MB (95.33%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -23,7 +23,37 @@ class Solution:
         #     return max(self.longestValidParentheses(s[:-1]), 
         #                 self.longestValidParentheses(s[1:]))         
 
-        # # Method 2: DP Tablelation
+        # # Method 1.1: recursion with memo
+        # if s in memo:
+        #     return memo[s]
+        # if len(s) == 1:
+        #     memo[s] = 0
+        #     return 0
+        # elif s == '()':
+        #     memo[s] = 2
+        #     return 2
+        # elif len(s) == 2 and s != '()':
+        #     memo[s] = 0
+        #     return 0    
+        # elif len(s) % 2 == 1:
+        #     return max(self.longestValidParentheses(s[:-1], memo), 
+        #     self.longestValidParentheses(s[1:], memo))      
+
+        # stack = []
+        # for i in range(len(s)):
+        #     if len(stack) > 0 and stack[-1] + s[i] == '()':
+        #         stack.pop(-1)
+        #     else:
+        #         stack.append(s[i])
+
+        # if len(stack) == 0:
+        #     memo[s] = len(s)
+        #     return memo[s]
+        # else:
+        #     return max(self.longestValidParentheses(s[:-1], memo), 
+        #                 self.longestValidParentheses(s[1:], memo))     
+
+        # # Method 2: DP Tabulation 2D
         # if len(s) == 0:
         #     return 0
         # dp = [[0] * len(s) for _ in range(len(s))]
@@ -59,20 +89,35 @@ class Solution:
                 
         # return res
 
-        # Method 3: Stack
+        # Method 2.2: DP Tabulation 1D
         if len(s) == 0:
             return 0
-        stack = [-1]
-        res = 0
-        for i in range(len(s)):
-            if i == 0:
-                stack.append(0)
-            elif len(stack) > 1 and s[stack[-1]] + s[i] == '()':
-                stack.pop() 
-                res = max(res, i - stack[-1])
-            else:
-                stack.append(i)    
-        return res
+        dp = [0 for _ in range(len(s))]
+        for i in range(1, len(s)):
+            if s[i-1:i+1] == '()':
+                dp[i] = dp[i-2] + 2
+            elif dp[i-1] != 0:
+                if i-dp[i-1]-1 >= 0 and s[i-dp[i-1]-1] + s[i] == '()':
+                    temp = dp[i-1] + 2
+                    dp[i] = temp + dp[i-temp]
+
+        return max(dp)              
+
+
+        # # Method 3: Stack
+        # if len(s) == 0:
+        #     return 0
+        # stack = [-1]
+        # res = 0
+        # for i in range(len(s)):
+        #     if i == 0:
+        #         stack.append(0)
+        #     elif len(stack) > 1 and s[stack[-1]] + s[i] == '()':
+        #         stack.pop() 
+        #         res = max(res, i - stack[-1])
+        #     else:
+        #         stack.append(i)    
+        # return res
 
 
 if __name__ == '__main__':
