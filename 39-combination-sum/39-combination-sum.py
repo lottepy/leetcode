577c7e9e9cce2bb1ce71dc65147ff57f8ed2c773
lottepy <98939371+lottepy@users.noreply.github.com>
2022-03-09 19:11:10 +0800

    Time: 107 ms (56.58%), Space: 14.6 MB (7.16%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,19 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        dp = [[] for _ in range(target)]
+        dp = [set() for _ in range(target)]
 
         for c in candidates:
             if c <= target:
-                dp[c-1].append([c])
+                dp[c-1].add(tuple([c]))
 
         for i in range(target):
             if i >= min(candidates):
                 for c in candidates:
                     if i-c >= 0 and len(dp[i-c]) > 0:
-                        temp = [sorted(s + [c]) for s in dp[i-c]]
-                        dp[i] += temp
+                        temp = set([tuple(sorted(list(s) + [c])) for s in dp[i-c]])
+                        dp[i] = temp.union(dp[i])
 
-        return [list(x) for x in set(tuple(x) for x in dp[-1])]               
-
+        return [list(t) for t in dp[-1]]  
 if __name__ == '__main__':
     print(Solution().combinationSum([2,3,6,7],7))
