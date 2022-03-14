21dc65e6d9df6799dbf40490a9602951e6983bb3
lottepy <98939371+lottepy@users.noreply.github.com>
2022-03-14 20:35:57 +0800

    Time: 854 ms (39.71%), Space: 25.6 MB (48.14%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,12 +1,26 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        accu = 0
-        res = sum(nums)
-        _min = 0
-        for i in range(len(nums)):
-            _min = min(accu, _min)
-            accu += nums[i]
-            res = max(res, accu - _min)
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # # Method 1: Accumulator
+        # accu = 0
+        # res = sum(nums)
+        # _min = 0
+        # for i in range(len(nums)):
+        #     _min = min(accu, _min)
+        #     accu += nums[i]
+        #     res = max(res, accu - _min)
 
-        return res     
-            
+        # return res     
+
+        # Method 2: sliding window
+        cur = 0
+        res = nums[0]
+        for n in nums:
+            if cur < 0:
+                cur = 0
+            cur += n   
+            res = max(res, cur)     
+        return res    
