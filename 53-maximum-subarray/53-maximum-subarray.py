bd13d5046d0286142d6d726f5036a7c2e75395af
lottepy <98939371+lottepy@users.noreply.github.com>
2022-03-14 20:57:11 +0800

    Time: 3657 ms (5.05%), Space: 24.9 MB (99.22%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -15,12 +15,32 @@ class Solution(object):
 
         # return res     
 
-        # Method 2: sliding window
-        cur = 0
-        res = nums[0]
-        for n in nums:
-            if cur < 0:
-                cur = 0
-            cur += n   
-            res = max(res, cur)     
-        return res    
+        # # Method 2: sliding window
+        # cur = 0
+        # res = nums[0]
+        # for n in nums:
+        #     if cur < 0:
+        #         cur = 0
+        #     cur += n   
+        #     res = max(res, cur)     
+        # return res    
+
+        # Method 3: D&C
+        if len(nums) == 1:
+            return nums[0]
+
+        def crossMax(myArr):
+            _max = myArr[0] 
+            _sum = 0   
+            for i in range(len(myArr)):
+                _sum += myArr[i]
+                _max = max(_max, _sum)
+            return _max
+
+        mid = len(nums) // 2
+        left = nums[:mid]
+        right = nums[mid:]
+        leftMax = self.maxSubArray(left)
+        rightmax = self.maxSubArray(right)
+        midMax = crossMax(right) + crossMax(left[::-1])
+        return max([leftMax, rightmax, midMax])
