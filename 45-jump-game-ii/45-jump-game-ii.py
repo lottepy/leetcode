0e2321ea0fc0f4320d9851b126482a0db6827000
lottepy <98939371+lottepy@users.noreply.github.com>
2022-03-17 20:29:48 +0800

    Time: 163 ms (76.18%), Space: 15.1 MB (68.06%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,8 +1,21 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        dp = list(range(len(nums)))
-        for i in range(len(nums)):
-            for step in range(1, nums[i]+1):
-                if i + step < len(nums):
-                    dp[i+step] = min(dp[i+step], dp[i]+1)  
-        return dp[-1] 
+        # # Method 1: Dynamic Programming
+        # dp = list(range(len(nums)))
+        # for i in range(len(nums)):
+        #     for step in range(1, nums[i]+1):
+        #         if i + step < len(nums):
+        #             dp[i+step] = min(dp[i+step], dp[i]+1)              
+        # return dp[-1]            
+
+        # Method 2: Greedy
+        res = 0
+        l, r = 0, 0
+        while r < len(nums) - 1:
+            nextRight = 0
+            for i in range(l, r+1):
+                nextRight = max(nextRight, i+nums[i])
+            l, r = r+1, nextRight
+            res += 1    
+        return res
+        
