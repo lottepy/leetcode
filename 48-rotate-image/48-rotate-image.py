8f00332b69508d1ab07ed9a46a639f7537a276a4
lottepy <98939371+lottepy@users.noreply.github.com>
2022-03-22 19:20:11 +0800

    Time: 72 ms (10.54%), Space: 31.8 MB (5.01%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -4,16 +4,31 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        def transpose(matrix):
-            n = len(matrix)
-            for i in range(n):
-                for j in range(i+1, n):
-                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        def flip(matrix):
-            n = len(matrix)
-            for j in range(n//2):
-                for i in range(n):     
-                    matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]  
+        # # Method 1: Decomposite
+        # def transpose(matrix):
+        #     n = len(matrix)
+        #     for i in range(n):
+        #         for j in range(i+1, n):
+        #             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        # def flip(matrix):
+        #     n = len(matrix)
+        #     for j in range(n//2):
+        #         for i in range(n):     
+        #             matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]  
 
-        transpose(matrix)
-        flip(matrix)      
+        # transpose(matrix)
+        # flip(matrix)
+
+        # Method 2: Direct replace              
+        n = len(matrix)
+        l, r = 0, n-1
+        while l < r:
+            for i in range(r-l):
+                top, bot = l, r
+                topLeft = matrix[top][l+i]
+                matrix[top][l+i] = matrix[bot-i][l]
+                matrix[bot-i][l] = matrix[bot][r-i]
+                matrix[bot][r-i] = matrix[top+i][r]
+                matrix[top+i][r] = topLeft
+            l += 1
+            r -= 1 
