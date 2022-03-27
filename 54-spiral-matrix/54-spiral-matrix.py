afed4c632e3747fd0b42abcd0ab5047608d3a9c9
lottepy <98939371+lottepy@users.noreply.github.com>
2022-03-27 18:38:55 +0800

    Time: 242 ms (5.48%), Space: 31.7 MB (7.93%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,20 +1,25 @@
 import numpy as np
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        matrix = np.array(matrix)
-        l, r = 0, len(matrix[0])-1
-        top, bottom = 0, len(matrix)-1
-        res = []
-        while l <= r and top <= bottom:
-            res += matrix[top, l:r+1].tolist()
-            res += matrix[top+1:bottom+1, r].tolist()
-            if bottom > top:
-                res += matrix[bottom,l:r][::-1].tolist()
-            if r > l:    
-                res += matrix[top+1:bottom,l][::-1].tolist()
-            l += 1
-            r -= 1
-            top += 1
-            bottom -= 1
-        return res    
+        # # Method 1: left right anchor
+        # matrix = np.array(matrix)
+        # l, r = 0, len(matrix[0])-1
+        # top, bottom = 0, len(matrix)-1
+        # res = []
+        # while l <= r and top <= bottom:
+        #     res += matrix[top, l:r+1].tolist()
+        #     res += matrix[top+1:bottom+1, r].tolist()
+        #     if bottom > top:
+        #         res += matrix[bottom,l:r][::-1].tolist()
+        #     if r > l:    
+        #         res += matrix[top+1:bottom,l][::-1].tolist()
+        #     l += 1
+        #     r -= 1
+        #     top += 1
+        #     bottom -= 1
+        # return res   
+
+        # Method 2: submatrix transform
+        return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])        
+  
         
