1542add1261e6cde0cd4a6948c4d5d3f2e5f6078
lottepy <98939371+lottepy@users.noreply.github.com>
2022-04-26 21:10:36 +0800

    Time: 66 ms (5.43%), Space: 13.9 MB (77.02%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -6,6 +6,6 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        def inorderTraversal(root: TreeNode, mark: str) -> List[int]:
-            return inorderTraversal(root.left, 'l') + inorderTraversal(root.right, 'r') + [root.val] if root else [mark]
-        return inorderTraversal(p, 'r') == inorderTraversal(q, 'r')  
+        def depthTraversal(root: TreeNode, mark: str) -> List[int]:
+            return depthTraversal(root.left, 'l') + depthTraversal(root.right, 'r') + [root.val] if root else [mark]
+        return depthTraversal(p, 'r') == depthTraversal(q, 'r')  
